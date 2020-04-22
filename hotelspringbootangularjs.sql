e400f5d2bec63d0c6ea2c5eb4608ded03bbf2ee2
SlobaZ <63258400+SlobaZ@users.noreply.github.com>
2020-04-22 19:26:54 +0200

    Update hotelspringbootangularjs.sql

--- hotelspringbootangularjs.sql
+++ hotelspringbootangularjs.sql
@@ -1,10 +1,10 @@
-CREATE USER IF NOT EXISTS jwduser IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS root IDENTIFIED BY 'root';
 
 DROP DATABASE IF EXISTS hotelspringbootangularjs;
 CREATE DATABASE hotelspringbootangularjs DEFAULT CHARACTER SET utf8;
 
 USE hotelspringbootangularjs;
 
-GRANT ALL ON hotelspringbootangularjs.* TO 'jwduser'@'%';
+GRANT ALL ON hotelspringbootangularjs.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
