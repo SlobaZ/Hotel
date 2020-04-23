bdf35d7b4c414a09a8e00e3e21d40c29e3048d3d
SlobaZ <63258400+SlobaZ@users.noreply.github.com>
2020-04-23 23:38:44 +0200

    Update PomocnaKlasa.java

--- src/main/java/hotel/utils/PomocnaKlasa.java
+++ src/main/java/hotel/utils/PomocnaKlasa.java
@@ -82,6 +82,42 @@ public class PomocnaKlasa {
 		double ukupnaCena = brojDana * cenaPoDanu ;
 		return ukupnaCena;	
 	}
+	
+	// Provera opsega Ulaz/Izlaz datuma
+	public static boolean odgovaraOpesegu(String ulaznaGranica, String izlaznaGranica) {
+		
+		boolean pripada = true;
+		Date ulazDatum = null;
+		Date izlazDatum = null;
+		Date danas = null;
+		String danasnjiDatum = null;
+
+		
+		Date datumDanas = new Date();
+		danasnjiDatum = DATE_TIME_FORMAT.format(datumDanas);
+		
+		try {
+			ulazDatum  = DATE_TIME_FORMAT.parse(ulaznaGranica);
+			izlazDatum = DATE_TIME_FORMAT.parse(izlaznaGranica);
+			danas = DATE_TIME_FORMAT.parse(danasnjiDatum);
+			
+			if(ulazDatum.before(izlazDatum) && !ulazDatum.before(danas)){
+	            pripada = true;
+	        }
+			else if(ulazDatum.before(danas)){
+				pripada=false;
+				
+			}
+			else {
+				pripada=false;
+				}
+			
+		} 
+		catch (ParseException e) {	
+		}
+	
+		return pripada;
+	}
 
 	
 }
