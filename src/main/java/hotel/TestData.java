080a6d481983dc370521d7e5123e0555b4ff9766
SlobaZ <63258400+SlobaZ@users.noreply.github.com>
2020-11-06 00:44:22 +0100

    Update TestData.java

--- src/main/java/hotel/TestData.java
+++ src/main/java/hotel/TestData.java
@@ -30,6 +30,31 @@ public class TestData {
 	public void init() {
 		
 		
+		//  GOSTI
+		
+		Gost gost1 = new Gost();
+		gost1.setNaziv("Pera Peric");
+		gost1.setMesto("Novi Sad");
+		gost1.setJmbg("1106970775533"); 
+		gost1.setTelefon("0641122333");
+		gost1 = gostService.save(gost1);
+		
+		
+		Gost gost2 = new Gost();
+		gost2.setNaziv("Jova Jovic");
+		gost2.setMesto("Beograd");
+		gost2.setJmbg("2208960335577"); 
+		gost2.setTelefon("0634455666");
+		gost2 = gostService.save(gost2);
+		
+		Gost gost3 = new Gost();
+		gost3.setNaziv("Vasa Vasic");
+		gost3.setMesto("Subotica");
+		gost3.setJmbg("0710950775533"); 
+		gost3.setTelefon("0657788999");
+		gost3 = gostService.save(gost3);
+	
+		
 		// SOBE
 		
 		Soba soba1 = new Soba();
@@ -40,11 +65,13 @@ public class TestData {
 		Soba soba2 = new Soba();
 		soba2.setNaziv("Single 02");
 		soba2.setBrojKreveta(1);
+		soba2.setGost(gost1);
 		soba2 = sobaService.save(soba2);
 
 		Soba soba3 = new Soba();
 		soba3.setNaziv("Double 01");
 		soba3.setBrojKreveta(2);
+		soba3.setGost(gost2);
 		soba3 = sobaService.save(soba3);
 
 		Soba soba4 = new Soba();
@@ -80,6 +107,7 @@ public class TestData {
 		Soba soba10 = new Soba();
 		soba10.setNaziv("Family room 02");
 		soba10.setBrojKreveta(4);
+		soba10.setGost(gost3);
 		soba10 = sobaService.save(soba10);
 
 		Soba soba11 = new Soba();
@@ -91,35 +119,7 @@ public class TestData {
 		soba12.setNaziv("Suite 02");
 		soba12.setBrojKreveta(2);
 		soba12 = sobaService.save(soba12);
-
-				
-		
-		
-		//  GOSTI
-		
-		Gost gost1 = new Gost();
-		gost1.setNaziv("Pera Peric");
-		gost1.setMesto("Novi Sad");
-		gost1.setJmbg("1106970775533"); 
-		gost1.setTelefon("0641122333");
-		gost1 = gostService.save(gost1);
-		
-		
-		Gost gost2 = new Gost();
-		gost2.setNaziv("Jova Jovic");
-		gost2.setMesto("Beograd");
-		gost2.setJmbg("2208960335577"); 
-		gost2.setTelefon("0634455666");
-		gost2 = gostService.save(gost2);
-		
-		Gost gost3 = new Gost();
-		gost3.setNaziv("Vasa Vasic");
-		gost3.setMesto("Subotica");
-		gost3.setJmbg("0710950775533"); 
-		gost3.setTelefon("0657788999");
-		gost3 = gostService.save(gost3);
 	
-		
 		
 		// REZERVACIJE
 		
@@ -129,7 +129,7 @@ public class TestData {
 		rezervacija1.setDatumvremeIzlaz(java.sql.Timestamp.valueOf("2020-04-23 10:10:10"));
 		rezervacija1.setDatetimeIzlaz("23.04.2020. 10:10");
 		rezervacija1.setGost(gost1);
-		rezervacija1.setSoba(soba3);
+		rezervacija1.setSoba(soba2);
 		rezervacija1 = rezervacijaService.save(rezervacija1);
 		
 		Rezervacija rezervacija2 = new Rezervacija();
@@ -138,7 +138,7 @@ public class TestData {
 		rezervacija2.setDatumvremeIzlaz(java.sql.Timestamp.valueOf("2020-03-19 14:30:10"));
 		rezervacija2.setDatetimeIzlaz("19.03.2020. 14:30");
 		rezervacija2.setGost(gost2);
-		rezervacija2.setSoba(soba4);
+		rezervacija2.setSoba(soba3);
 		rezervacija2 = rezervacijaService.save(rezervacija2);
 		
 		Rezervacija rezervacija3 = new Rezervacija();
